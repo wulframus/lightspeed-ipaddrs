4ac578dbf04b3cc9421cd8d5c64f1b0a77e1e639
Nikolai A. Dubinin <wulframus@gmail.com>
2024-10-10 09:35:32 +0400

    Commit

--- main.go
+++ main.go
@@ -11,22 +11,24 @@ import (
 type Bucket interface {
 	Add(ip net.IP) error
 	IsFull() bool
-	Count() uint
+	Count() int
+	FullBuckets() int
+	addRecursive(ip net.IP, depth int) int
 }
 
 type BucketFull struct {
 	counter int
+	matches int
 }
 
 type BucketFeed struct {
 	counter int
-	depth int
 	fullBucketsCounter int
 	buckets [256]Bucket
 }
 
-func newBucketFull(counter uint) *BucketFull {
-	return &BucketFull{counter: int(counter)}
+func newBucketFull(counter int) *BucketFull {
+	return &BucketFull{counter: counter}
 }
 
 func newBucketFeed() *BucketFeed{
@@ -44,21 +46,39 @@ func (b *BucketFull) IsFull() bool {
 	return true
 }
 
+func (b *BucketFull) FullBuckets() int {
+	return 256
+}
+
+func (b *BucketFull) addRecursive(ip net.IP, depth int) int {
+	ret := 0
+	if b.matches == 0 {
+		ret = 1
+	} else if b.matches == 1 {
+		ret = -1
+	}
+	b.matches++
+	return ret
+}
+
 func (b *BucketFull) Add(ip net.IP) error {
 	return nil // Bucket is full, nothing to add
 }
 
 
-func (b *BucketFull) Count() uint {
-	return uint(b.counter)
+func (b *BucketFull) Count() int {
+	return b.counter
 }
 
 // Interface implementation for BucketFeed
 
 func (b *BucketFeed) IsFull() bool {
-	return b.fullBucketsCounter == 256
+	return false
 }
 
+func (b *BucketFeed) FullBuckets() int {
+	return b.fullBucketsCounter
+} 
 /**
  * returns 1 if address added, -1 if occurs once, 0 if occurs more times
  */
@@ -75,11 +95,16 @@ func (b *BucketFeed) addRecursive(ip net.IP, depth int) int {
 		octet := ip[0]
 		remain := ip[1:]
 		if b.buckets[octet] == nil {
-			b.buckets[octet] = newBucketFeed()
+			if(depth > 1) {
+				b.buckets[octet] = newBucketFeed()
+			} else {
+				b.buckets[octet] = newBucketFull(0)
+				b.fullBucketsCounter++
+			}
 		}
-		ret = b.buckets[octet].(*BucketFeed).addRecursive(remain, depth - 1)
+		ret = b.buckets[octet].addRecursive(remain, depth - 1)
 		b.counter += ret
-		if b.buckets[octet].IsFull() {
+		if !b.buckets[octet].IsFull() && b.buckets[octet].FullBuckets() == 256 {
 			counter := b.buckets[octet].Count()
 			b.buckets[octet] = newBucketFull(counter)
 			b.fullBucketsCounter++
@@ -100,8 +125,8 @@ func (b *BucketFeed) Add(ip net.IP) error {
 	return nil
 }
 
-func (b *BucketFeed) Count() uint {
-	return uint(b.counter)
+func (b *BucketFeed) Count() int {
+	return b.counter
 }
 
 func main() {
@@ -127,12 +152,12 @@ func main() {
 		}
 		ip := net.ParseIP(line)
 		if ip == nil {
-			fmt.Printf("Incorrect IP address: %s\n", line)
+			// fmt.Printf("Incorrect IP address: %s\n", line)
 			continue
 		}
 		err := root.Add(ip)
 		if err != nil {
-			fmt.Printf("Couldn't add %s: go run -race%v\n", line, err)
+			// fmt.Printf("Couldn't add %s: %v\n", line, err)
 			continue
 		}
 	}
