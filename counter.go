28e74aedb439af806edd0c768b1ecd5525371932
Nikolai A. Dubinin <wulframus@gmail.com>
2024-10-11 16:27:24 +0400

    BitSet solution

---
+++ counter.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"net"
+	"github.com/bits-and-blooms/bitset"
+)
+
+const (
+	storageSize = (1 << 32) // Length of storage enough for all IPv4 addresses range 
+)
+
+type UniqueIpv4Counter struct {
+	storage *bitset.BitSet
+	counter uint
+}
+
+func v4ToUInt32(ip net.IP) uint32 {
+	return (uint32(ip[0]) << 24) | (uint32(ip[1]) << 16) | (uint32(ip[2]) << 8) | uint32(ip[3])
+}
+
+func NewUniqueIpv4Counter() *UniqueIpv4Counter {
+	return &UniqueIpv4Counter{
+		storage: bitset.New(storageSize),
+		counter: 0,
+	}
+}
+
+func (c *UniqueIpv4Counter) addUInt32(i uint32) {
+	if !c.storage.Test(uint(i)) {
+		c.storage.Set(uint(i))
+		c.counter++
+	}
+}
+
+func (c *UniqueIpv4Counter) Add(ip string) {
+	parsedIp := net.ParseIP(ip)
+	parsedIp = parsedIp.To4()
+	c.addUInt32(v4ToUInt32(parsedIp))
+}
+
+func (c *UniqueIpv4Counter) Clear() {
+	c.storage.ClearAll()
+	c.counter = 0
+}
+
+func (c *UniqueIpv4Counter) Count() uint {
+	return c.counter
+}
+
--- main.go
+++ main.go
@@ -5,89 +5,8 @@ import (
 	"net"
 	"os"
 	"bufio"
-	"strings"
+	// "strings"
 )
-
-type Tree struct {
-	leftCount int
-	leftIsFull bool
-	leftSubTree *Tree
-	rightCount int
-	rightIsFull bool
-	rightSubTree *Tree
-}
-
-func NewTree() *Tree {
-	return &Tree{}
-}
-
-func (self *Tree) IsFull() bool {
-	return self.leftIsFull && self.rightIsFull
-}
-
-func (self *Tree) IsEmpty() bool {
-	return !self.leftIsFull && !self.rightIsFull
-}
-
-func (self *Tree) Count() uint {
-	return uint(self.leftCount + self.rightCount)
-}
-
-func (self *Tree) addRecursive(ip uint32, depth int) int {
-	if self.IsFull() {
-		return 0
-	}
-	ret := 0
-	if depth == 0 {
-		if self.IsEmpty() {
-			ret = 1
-			self.rightIsFull = true
-		} else {
-			self.leftIsFull = true
-			ret = -1
-		}
-	} else {
-		if (ip & 1) == 0 {
-			if self.rightIsFull {
-				return ret
-			}
-			if self.rightSubTree == nil {
-				self.rightSubTree = NewTree()
-			}
-			ret = self.rightSubTree.addRecursive(ip >> 1, depth - 1)
-			self.rightCount += ret
-			if self.rightSubTree.IsFull() {
-				self.rightIsFull = true
-				self.rightSubTree = nil
-			}
-		} else {
-			if self.leftIsFull {
-				return ret
-			}
-			if self.leftSubTree == nil {
-				self.leftSubTree = NewTree()
-			}
-			ret = self.leftSubTree.addRecursive(ip >> 1, depth - 1)
-			self.leftCount += ret
-			if self.leftSubTree.IsFull() {
-				self.leftIsFull = true
-				self.leftSubTree = nil
-			}
-		}
-	}
-	return ret
-}
-
-func toUint32(ip net.IP) uint32 {
-	return (uint32(ip[0]) << 24) | (uint32(ip[1]) << 16) | (uint32(ip[2]) << 8) | uint32(ip[3])
-}
-
-func (self *Tree) Add(ip net.IP) {
-	// ip4 := toUint32(ip)
-	// ret := self.addRecursive(toUint32(ip), 32)
-	// fmt.Printf("%d %08x %v\n", ret, ip4, ip)
-	self.addRecursive(toUint32(ip), 32)
-}
 
 func main() {
 	if len(os.Args) < 2 {
@@ -103,27 +22,16 @@ func main() {
 	defer file.Close()
 
 	scanner := bufio.NewScanner(file)
-	root := NewTree()
+	counter := NewUniqueIpv4Counter()
 
 	for scanner.Scan() {
-		line := strings.TrimSpace(scanner.Text())
-		if line == "" {
-			continue
-		}
-		ip := net.ParseIP(line)
-		if ip == nil {
-			continue
-		}
-		ip = ip.To4()
-		if ip == nil {
-			continue
-		}
-		root.Add(ip)
+		line := scanner.Text()
+		counter.Add(line)
 	}
 
 	if err := scanner.Err(); err != nil {
 		fmt.Printf("Error reading file: %v\n", err)
 	}
 
-	fmt.Printf("Unique IP addresses count: %d\n", root.Count())
+	fmt.Printf("Unique IP addresses count: %d\n", counter.Count())
 }
